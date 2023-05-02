05ef499f3fc376d4a990959486c893f9990b88cf
Jaesung Lim <wotjd2732@gmail.com>
2023-05-02 02:40:25 -0400

    JS: Projects filtering

--- main.js
+++ main.js
@@ -50,6 +50,29 @@ arrowUpBtn.addEventListener('click', () => {
   scrollIntoView('#home');
 });
 
+// Projects filter
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projectContainer.classList.add('animation-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+      projectContainer.classList.remove('animation-out');
+    });
+  }, 300);
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
